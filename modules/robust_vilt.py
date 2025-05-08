c5566c5f53a5a32bea2d58e640f6a279cb56413a
zagarsuren <zagarsuren@gmail.com>
2025-05-08 16:24:57 +1000

    add local model path

--- modules/robust_vilt.py
+++ modules/robust_vilt.py
@@ -7,7 +7,7 @@ class RobustViLT:
     Robust Vision-and-Language Transformer (ViLT) for VQA.
     Uses a pretrained checkpoint (default "dandelin/vilt-b32-finetuned-vqa").
     """
-    def __init__(self, model_name="dandelin/vilt-b32-finetuned-vqa"):
+    def __init__(self, model_name="/Users/zagaraa/Documents/GitHub/visionaid-vqa/models/vilt_finetuned_vizwiz"):
         self.processor = ViltProcessor.from_pretrained(model_name)
         self.model = ViltForQuestionAnswering.from_pretrained(model_name)
         # self.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu") 
